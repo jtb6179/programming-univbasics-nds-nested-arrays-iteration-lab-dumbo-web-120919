718542ccc60d01864867467ab0b30bd8ecc6a0a3
Joel  Bly <jtb6179@g.rit.edu>
2019-12-02 01:29:20 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -51,7 +51,7 @@ def total_even_pairs(src)
   ti = []
   qp = 0 
   while qp < src.count do 
-    if src[qp][0] && src[qp][1] % 2 == 0 
+    if src[qp][0]% 2 == 0 && src[qp][1] % 2 == 0 
       ti << src[qp][0] && src[qp][1]
     elsif src[qp][0] % 2 == 0
     ti << src[qp][0] 
